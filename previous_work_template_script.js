f76cfe14dfc7e46bf911390cdba131267d2bc724
Daniel Repko <danielrepko07@gmail.com>
2020-11-14 16:09:27 -0500

    Overhaul Site Design *deleted the old code and replaced it with the code from my site I made at St. Clair College *removed sensitive information such as references and my phone number

---
+++ previous_work_template_script.js
@@ -0,0 +1,14 @@
+//loading the page and naming the window connect_four
+function loadConnectFour() {
+	window.open("previous_work_files/portfolio_previous_work.html", "connect_four");
+}
+
+//loading the page and naming the window pizza_palace
+function loadPizzaPalace() {
+	window.open("previous_work_files/portfolio_previous_work.html", "pizza_palace");
+}
+
+//loading the page and naming the window step_ahead
+function loadStepAhead() {
+	window.open("previous_work_files/portfolio_previous_work.html", "step_ahead");
+}
